7ded8d0711f7cf1d618f471688159ab62d4f2910
harryi3t <66hari@gmail.com>
2017-01-17 02:13:09 +0530

    adds pdf to ignore list in grep

--- grep.sh
+++ grep.sh
@@ -1,6 +1,7 @@
 alias ngrep="grep -nr \
   --exclude=\*.{min,cat}.js \
   --exclude=\*.log \
+  --exclude=\*.pdf \
   --exclude-dir=\*node_modules\* \
   --exclude-dir=\*assets\* \
   --exclude-dir=\*node_modules\* \

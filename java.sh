f9a802254509c1001a010ccfab004252f14e7a07
harryi3t <66hari@gmail.com>
2017-01-17 02:13:47 +0530

    updates java path + adds android home and jre home

--- java.sh
+++ java.sh
@@ -1,9 +1,8 @@
-JAVA_HOME=/usr/local/java/jdk1.8.0_102
+JAVA_HOME=/usr/local/java/jdk1.8.0_112
 JRE_HOME=$JAVA_HOME/jre
-PATH=$PATH:$JAVA_HOME/bin:$JRE_HOME/bin
 ANDROID_HOME=/home/harry/Android/Sdk
 export JAVA_HOME
 export JRE_HOME
-export PATH
 export ANDROID_HOME
+export PATH=${PATH}:$JAVA_HOME/bin:$JRE_HOME/bin:${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools
 

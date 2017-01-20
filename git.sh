ceccc9e9a7e7e4e71a7b4401f7ec345eeee61d22
harryi3t <66hari@gmail.com>
2017-01-20 15:23:05 +0530

    updates the delete webhook to exclude some string useful for excluding current ngrok url which is active

--- git.sh
+++ git.sh
@@ -6,14 +6,23 @@ alias gm="git add . && git commit --amend --no-edit"
 
 function webhookDelete() {
   REPO="$1"
+  NEW_NGROK_URL="$2"
+  MESSAGE="${RED}deleting for url $URL ${NORMAL}"
+  if [ "$NEW_NGROK_URL" == '' ];then
+    NEW_NGROK_URL='somerandomstringwhichwillnevermatch'
+    MESSAGE="$MESSAGE\n\n"
+  else
+    MESSAGE="$MESSAGE\n${YELLOW}Ignoring the urls containing $NEW_NGROK_URL \n\n${NORMAL}"
+  fi
+
   URL="https://api.github.com/repos/$REPO/hooks?access_token=$TOKEN"
-  ​
-  echo "${RED}deleting for url $URL \n\n${NORMAL}"
-  curl $URL | grep '"id"' | awk -F , '{print $1}' | awk '{print $2}' | while read id
+
+  echo "$MESSAGE"
+
+  curl $URL | grep --exclude-dir={.bzr,CVS,.git,.hg,.svn} -E '("id"|172.17.42.1|ngrok)' |  grep -v "$NEW_NGROK_URL" | grep '"url"' -B 1 | grep '"id"' | awk -F , '{print $1}' | awk '{print $2}' | while read id
   do
-     DELETE_URL="https://api.github.com/repos/$REPO/hooks/$id?access_token=$TOKEN"
-     echo "${YELLOW}Deleting $id${NORMAL}"
-     curl -X DELETE $DELETE_URL
-  ​
-   done
+    DELETE_URL="https://api.github.com/repos/$REPO/hooks/$id?access_token=$TOKEN"
+    echo "${YELLOW}Deleting $id${NORMAL}"
+    curl -X DELETE $DELETE_URL
+  done
 }

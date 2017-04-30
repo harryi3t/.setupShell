c6c64218e7d75f0c2125010b5c3f3734f2ec8109
harryi3t <66hari@gmail.com>
2017-04-30 22:59:58 +0530

    adds auto install: -y flag

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-sudo apt-get install zsh
+sudo apt-get install zsh -y
 
 # f: fail, s:silent, S: show error, L: follow redirect
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"

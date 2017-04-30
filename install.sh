7fa44ef04f954ff0d09c352c6d40fe92e097ece1
harryi3t <66hari@gmail.com>
2017-04-30 23:02:56 +0530

    dont show error

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 sudo apt-get install zsh -y
 
-# f: fail, s:silent, S: show error, L: follow redirect
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# f: fail, s:silent, L: follow redirect
+sh -c "$(curl -fsL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"

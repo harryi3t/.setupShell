b825306e6e0b784c054bd26195368b9e6c340c4c
harryi3t <66hari@gmail.com>
2017-04-30 23:24:13 +0530

    creates a new filw zshrc-plugin that has to be sourced in zshrc file

--- README.md
+++ README.md
@@ -2,7 +2,13 @@
 includes the basic alias and settings I need after installing a new Linux machine
 
 # Basic Installation
-## Install zsh & oh-my-zsh
+
+### This will install zsh and oh-my-zsh
 ```bash
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/harryi3t/.setupShell/master/install.sh)"
+```
+
+### This will source the plugins into your zshrc file
+```bash
+echo "source ~/.zshrc-plugin" >> ~/.zshrc
 ```
--- install.sh
+++ install.sh
@@ -3,14 +3,12 @@
 sudo apt-get install zsh -y
 rm -rf ~/.oh-my-zsh
 
-touch ~/.zshrc
-echo ""                                                          >> ~/.zshrc
-echo "# Added automatically from script by harryi3t"             >> ~/.zshrc
-echo "plugins=(git z zsh-completions)"                           >> ~/.zshrc
-echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
-echo "source ~/.oh-my-zsh/plugins/z/z.sh"                        >> ~/.zshrc
+touch ~/.zshrc-plugin
+echo ""                                                          >> ~/.zshrc-plugin
+echo "# Added automatically from script by harryi3t"             >> ~/.zshrc-plugin
+echo "plugins=(git z zsh-completions)"                           >> ~/.zshrc-plugin
+echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc-plugin
+echo "source ~/.oh-my-zsh/plugins/z/z.sh"                        >> ~/.zshrc-plugin
 
 # f: fail, s:silent, S:show error, L: follow redirect
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-source ~/.zshrc

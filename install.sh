524b33fcffc455961a4d41d19d24fc24c8acf54f
harryi3t <66hari@gmail.com>
2017-04-30 23:36:52 +0530

    clones the missing zsh-autocompletion plugin

--- install.sh
+++ install.sh
@@ -2,10 +2,16 @@
 
 sudo apt-get install zsh -y
 rm -rf ~/.oh-my-zsh
+rm ~/.zshrc-plugin || true
+
+mkdir ~/.zsh || true
+if [ ! -d "$HOME/.zsh/zsh-autosuggestions" ]; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.zsh/zsh-autosuggestions
+fi
 
 touch ~/.zshrc-plugin
-echo ""                                                          >> ~/.zshrc-plugin
 echo "# Added automatically from script by harryi3t"             >> ~/.zshrc-plugin
+echo ""                                                          >> ~/.zshrc-plugin
 echo "plugins=(git z zsh-completions)"                           >> ~/.zshrc-plugin
 echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc-plugin
 echo "source ~/.oh-my-zsh/plugins/z/z.sh"                        >> ~/.zshrc-plugin

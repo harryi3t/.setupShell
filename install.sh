b08ba5b2d7d822df142ee9ca0b47b542ff03c373
harryi3t <66hari@gmail.com>
2017-04-30 23:04:17 +0530

    adds an ignore message

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ sudo apt-get install zsh -y
 
 # f: fail, s:silent, L: follow redirect
 sh -c "$(curl -fsL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+echo "ignore if you get the error: 'You'll need to remove ~/.oh-my-zsh if you want to re-install'"

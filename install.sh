3b801b5992f2466fc302ad9b791a97f9bc97557d
harryi3t <66hari@gmail.com>
2017-04-30 23:13:48 +0530

    adds test comands

--- install.sh
+++ install.sh
@@ -1,15 +1,21 @@
-#!/bin/bash
+#!/bin/bash -e
 
 sudo apt-get install zsh -y
 rm -rf ~/.oh-my-zsh
 
-# f: fail, s:silent, L: follow redirect
-sh -c "$(curl -fsL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# f: fail, s:silent, S:show error, L: follow redirect
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+echo "last 5 lines of zshrc files before"
+cat ~/.zshrc | tail -5
 
 echo ""                                                          >> ~/.zshrc
 echo "# Added automatically from script by harryi3t"             >> ~/.zshrc
 echo "plugins=(git z zsh-completions)"                           >> ~/.zshrc
 echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
 echo "source ~/.oh-my-zsh/plugins/z/z.sh"                        >> ~/.zshrc
+
+echo "last 5 lines of zshrc files after"
+cat ~/.zshrc | tail -5
 
 source ~/.zshrc

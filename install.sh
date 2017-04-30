b73a9cb851413a9339670875c5bc5bc33dcea357
harryi3t <66hari@gmail.com>
2017-04-30 23:16:48 +0530

    creates zshrc files before installing oh-my-zsh

--- install.sh
+++ install.sh
@@ -3,19 +3,14 @@
 sudo apt-get install zsh -y
 rm -rf ~/.oh-my-zsh
 
-# f: fail, s:silent, S:show error, L: follow redirect
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-echo "last 5 lines of zshrc files before"
-cat ~/.zshrc | tail -5
-
+touch ~/.zshrc
 echo ""                                                          >> ~/.zshrc
 echo "# Added automatically from script by harryi3t"             >> ~/.zshrc
 echo "plugins=(git z zsh-completions)"                           >> ~/.zshrc
 echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
 echo "source ~/.oh-my-zsh/plugins/z/z.sh"                        >> ~/.zshrc
 
-echo "last 5 lines of zshrc files after"
-cat ~/.zshrc | tail -5
+# f: fail, s:silent, S:show error, L: follow redirect
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 source ~/.zshrc

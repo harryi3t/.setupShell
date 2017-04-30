7484693c9e182220fea7ec96ad98c13eba9c45da
harryi3t <66hari@gmail.com>
2017-04-30 23:09:06 +0530

    adds z & auto-completion plugins

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
-#!/bin/bash -e
+#!/bin/bash
 
 sudo apt-get install zsh -y
+rm -rf ~/.oh-my-zsh
 
 # f: fail, s:silent, L: follow redirect
 sh -c "$(curl -fsL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-echo "ignore if you get the error: 'You'll need to remove ~/.oh-my-zsh if you want to re-install'"
+echo ""                                                          >> ~/.zshrc
+echo "# Added automatically from script by harryi3t"             >> ~/.zshrc
+echo "plugins=(git z zsh-completions)"                           >> ~/.zshrc
+echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
+echo "source ~/.oh-my-zsh/plugins/z/z.sh"                        >> ~/.zshrc
+
+source ~/.zshrc

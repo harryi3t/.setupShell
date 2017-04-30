39a388d661439c99570ec9043ade02ea0fd246d2
harryi3t <66hari@gmail.com>
2017-04-30 22:53:57 +0530

    adds a install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -e
+
+sudo apt-get install zsh
+
+# f: fail, s:silent, S: show error, L: follow redirect
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
